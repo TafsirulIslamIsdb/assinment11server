a5e0917e2031423f6c08569a84bcc44610a04638
TafsirulIslamIsdb <103914080+TafsirulIslamIsdb@users.noreply.github.com>
2023-11-07 21:00:21 +0600

    adding job related api

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express();
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5001;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId  } = require('mongodb');
+
 app.use(cors({
   origin: [
-    'http://localhost:5173',
-
-    // 'https://cars-doctor-6c129.web.app',
-    // 'https://cars-doctor-6c129.firebaseapp.com'
+    'http://127.0.0.1:5173',
+    // 'http://localhost:5001',
+    'http://localhost:5173'
   ],
   credentials: true
 }));
@@ -33,11 +33,33 @@ const client = new MongoClient(uri, {
   }
 });
 
+const logger = (req, res, next) => {
+  console.log('log: info', req.method, req.url);
+  next();
+}
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  // console.log('token in the middleware', token);
+  // no token available 
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded;
+    next();
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const addjobCollection = client.db('JobsDB').collection('jobs');
+    //const myjobCollection = client.db('JobsDB').collection('myjobs');
 
 
     app.post('/jwt', async (req, res) => {
@@ -59,13 +81,76 @@ async function run() {
       res.clearCookie('token', { maxAge: 0 }).send({ success: true })
     })
 
-    app.post('/addjobs', async (req, res) => {
+    app.post('/jobs', async (req, res) => {
       const newJob = req.body;
       console.log(newJob);
       const result = await addjobCollection.insertOne(newJob);
       res.send(result);
       console.log("This is the post   Api");
     })
+
+    app.put('/jobs/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: id }
+      const options = { upsert: true };
+      const updatedjobs = req.body;
+      console.log("This is the put  Api");
+      
+      //const { _id,email, jobTitle, description, category, minPrice, maxPrice } = job || {};
+
+      const jobs = {
+        $set: {
+          email: updatedjobs.email,
+          jobTitle: updatedjobs.jobTitle,
+          description: updatedjobs.description,
+          category: updatedjobs.category,
+          minPrice: updatedjobs.minPrice,
+          maxPrice: updatedjobs.maxPrice
+        }
+      }
+
+      const result = await addjobCollection.updateOne(filter, jobs, options);
+      res.send(result);
+      console.log("tafsir");
+    })
+
+    app.delete('/jobs/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log("deleted id",id);
+      const query = {_id: new ObjectId(id) }
+      const result = await addjobCollection.deleteOne(query);
+     
+      
+      res.send(result);
+      
+  })
+
+    app.get('/jobs/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await addjobCollection.findOne(query);
+      res.send(result);
+      console.log("This is the single item finding   Api");
+    })
+
+    app.get('/jobs', async (req, res) => {
+      const cursor = addjobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    app.get('/jobs', async (req, res) => {
+
+      console.log(req.query.email);
+      console.log('token owner info', req.user)
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await addjobCollection.find(query).toArray();
+      res.send(result);
+
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
